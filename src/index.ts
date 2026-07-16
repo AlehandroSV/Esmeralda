a0dd9bd9bc5cd64199690102ae7919a3c3a99282
AlehandroSV <alehandrosantos99@gmail.com>
2026-07-16 08:33:41 -0300

    fix: replace import.meta.url with __dirname for CommonJS compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 import { Command } from "commander";
 import { readFileSync } from "fs";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
+import { join } from "path";
 import { registerInit } from "./cli/init.js";
 import { registerGenerate } from "./cli/generate.js";
 import { registerMigrate } from "./cli/migrate.js";
@@ -12,8 +11,6 @@ import { registerDbPull } from "./cli/db-pull.js";
 import { registerDbPush } from "./cli/db-push.js";
 import { registerSeed } from "./cli/seed.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 const pkg = JSON.parse(readFileSync(join(__dirname, "..", "package.json"), "utf-8"));
 
 const program = new Command();
